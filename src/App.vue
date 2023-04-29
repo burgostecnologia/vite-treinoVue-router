9ca5fbb6afdec652cf7cf473d958e3eee6aca015
Roberto Burgos <burgostecnologia@gmail.com>
2023-04-29 11:19:55 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <div>
+    <h1>Header</h1>
+    <nav>
+      <router-link to="/">Home</router-link>&nbsp;
+      <router-link to="/about">About</router-link>&nbsp;
+      <router-link to="/movie">Movie</router-link>&nbsp;
+      <RouterLink :to="rotaDinamica">Serviço</RouterLink>
+    </nav>
+    <RouterView></RouterView>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+   
+</div>
+</template>
+
+<script>
+export default{
+  data () {
+    return {
+      rotaDinamica: {name: 'servico'}
+    }
+  },
+  
+}
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import './style.css'
+
+import App from './App.vue'
+
+import Home from './views/home.vue'
+import About from './views/about.vue'
+//import Movie from './views/movie.vue'
+
+const routes = [
+    {path: '/', component: Home},
+    {path: '/about', component: About},
+  //  {path: '/movie', component: Movie}
+    {path: '/movie', component: () => import('./views/movie.vue')}, //assim só carrega quando acessar , + performatico
+    {path: '/servico', name: 'servico', component: () => import('./views/servico.vue')}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+createApp(App).use(router).mount('#app')
